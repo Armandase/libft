22fb05ce147d948950f194e50bd5542f47fbe9e9
Armand Damiens <adamiens@3C5.42angouleme.fr>
2022-09-29 17:16:59 +0200

    psuh

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: adamiens <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:46:28 by adamiens          #+#    #+#             */
-/*   Updated: 2022/09/29 16:59:07 by adamiens         ###   ########.fr       */
+/*   Updated: 2022/09/29 17:15:43 by adamiens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_totalen(char const *s, char c)
 {
 	int	i;
-	int counter;
+	int	counter;
 
 	counter = 1;
 	i = 0;
@@ -40,12 +40,11 @@ int	ft_wordlen(const char *str, char c, int i)
 
 	counter = 0;
 	while (str[counter + i] != c)
-	{
 		counter++;
-	}
 	return (counter);
 }
-char **ft_split(char const *s, char c)
+
+char	**ft_split(char const *s, char c)
 {
 	char	**strs;
 	int		i;
@@ -61,38 +60,27 @@ char **ft_split(char const *s, char c)
 		j = 0;
 		while (s[i] == c)
 			i++;
-		if (!s[i])
+		if (s[i] == '\0')
 			break ;
-		strs[k] = malloc(sizeof(char) * ft_wordlen(s, c, i));
+		strs[k] = malloc(ft_wordlen(s, c, i) + 1);
 		while (s[i] != c && s[i])
-		{
-			strs[k][j] = s[i];
-			i++;
-			j++;
-		}
+			strs[k][j++] = s[i++];
+		strs[k][j] = '\0';
 		k++;
 	}
 	strs[k] = 0;
 	return (strs);
 }
-#include <unistd.h>
 #include <stdio.h>
 int main(int argc, char **argv)
 {
-	(void)argc;
+    (void)argc;
     char    **strs;
-    int     i;
-    const char  *str1 = "test        ";
-    char    sep = ' ';
-
-    strs = ft_split(str1, sep);
-    i = 0;
-	int size = ft_totalen(argv[1], argv[2][0]);
-    while (i < size - 1)
+    int j = ft_totalen(argv[1], argv[2][0]);
+    strs = ft_split(argv[1], argv[2][0]);
+    for(int i = 0; i < j; i++)
     {
-        puts(strs[i]);
-        puts("-");
-        i++;
+        printf("%s\n", strs[i]);
     }
     return (0);
 }
